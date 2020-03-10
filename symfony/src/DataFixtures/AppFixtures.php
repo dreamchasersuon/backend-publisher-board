3c626a6e9483430c2928f743ae5014a8ba1d3f97
Tim <formulatim@gmail.com>
2020-03-10 18:56:17 +0300

    add tests and fixtures, configuring testing env

--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -15,14 +15,12 @@ class AppFixtures extends Fixture
 
     private function loadUsers(ObjectManager $manager): void
     {
-        foreach ($this->getUserPersonal() as [$user_id, $user_name, $user_custom, $email, $balance, $register_date, $enabled]) {
+        foreach ($this->getUserPersonal() as [$user_id, $user_name, $user_custom, $email]) {
             $user = new User();
             $user->setUserId($user_id);
             $user->setUserName($user_name);
             $user->setUserCustom($user_custom);
             $user->setEmail($email);
-            $user->setBalance($balance);
-            $user->setRegisterDate($register_date);
 
             $manager->persist($user);
         }
@@ -32,9 +30,9 @@ class AppFixtures extends Fixture
     private function getUserPersonal(): array
     {
         return [
-            ['1', 'Jane Doe', 'jane_admin', 'jane_admin@symfony.com', 0, '2020-03-10T08:14:03Z', true],
-            ['2', 'Tom Doe', 'tom_admin', 'tom_admin@symfony.com', 100, '2020-01-10T10:23:51Z', true],
-            ['3', 'John Doe', 'john_user', 'john_user@symfony.com', 2000, '2018-03-09T09:19:23Z', true]
+            ['1', 'Jane Doe', 'jane_admin', 'jane_admin@symfony.com'],
+            ['2', 'Tom Doe', 'tom_admin', 'tom_admin@symfony.com'],
+            ['3', 'John Doe', 'john_user', 'john_user@symfony.com']
         ];
     }
 }
--- symfony/tests/Controller/UserControllerTest.php
+++ symfony/tests/Controller/UserControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserControllerTest extends WebTestCase
 {
@@ -37,5 +38,152 @@ class UserControllerTest extends WebTestCase
         yield ['POST', self::API_URL . '/users', [], [], [], '{ "user_id": "4" }'];
         yield ['PUT', self::API_URL . '/users/2', [], [], [], '{ "user_name": "Woland" }'];
         yield ['POST', self::API_URL . '/users/3/recharge', [], [], [], '{ "balance": 100 }'];
+    }
+
+    public function testGetUsersInvalidUrl()
+    {
+        $client = static::createClient();
+        $client->request('GET', self::API_URL . '/users');
+        $this->assertEquals(
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testGetUsersSuccessful()
+    {
+        $client = static::createClient();
+        $client->request('GET', self::API_URL . '/users?offset=0&limit=10');
+
+        $this->assertEquals(
+            Response::HTTP_OK,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testCreateUserInvalidBody()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            self::API_URL . '/users',
+            [],
+            [],
+            [],
+            '{ "user_name": "Alan" }'
+        );
+
+        $this->assertEquals(
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testCreateUserAlreadyExist()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            self::API_URL . '/users',
+            [],
+            [],
+            [],
+            '{ "user_id": "1", "user_name": "Paul" }'
+        );
+
+        $this->assertEquals(
+            Response::HTTP_CONFLICT,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testCreateUserSuccessful()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            self::API_URL . '/users',
+            [],
+            [],
+            [],
+            '{ "user_id": "10", "user_name": "Paul" }'
+        );
+
+        $this->assertEquals(
+            Response::HTTP_CREATED,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testUpdateUserNotFound()
+    {
+        $client = static::createClient();
+        $client->request(
+            'PUT',
+            self::API_URL . '/users/100',
+            [],
+            [],
+            [],
+            '{ "user_name": "Luck" }'
+        );
+
+        $this->assertEquals(
+            Response::HTTP_NOT_FOUND,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testUpdateUserSuccessful()
+    {
+        $client = static::createClient();
+        $client->request(
+            'PUT',
+            self::API_URL . '/users/1',
+            [],
+            [],
+            [],
+            '{ "user_name": "Luck" }'
+        );
+
+        $this->assertEquals(
+            Response::HTTP_NO_CONTENT,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testUpdateUserBalanceNotFound()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            self::API_URL . '/users/100/recharge',
+            [],
+            [],
+            [],
+            '{ "balance": 2500 }'
+        );
+
+        $this->assertEquals(
+            Response::HTTP_NOT_FOUND,
+            $client->getResponse()->getStatusCode()
+        );
+    }
+
+    public function testUpdateUserBalanceSuccessful()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            self::API_URL . '/users/2/recharge',
+            [],
+            [],
+            [],
+            '{ "balance": 2500 }'
+        );
+
+        $this->assertEquals(
+            Response::HTTP_NO_CONTENT,
+            $client->getResponse()->getStatusCode()
+        );
     }
 }
